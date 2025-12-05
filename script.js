e0f41baeea0fe5f845631840d050b6b7a1a8774c
Puneet Arora <arorapuneet234@gmail.com>
2025-12-05 21:03:04 +0530

    Created Ui basic version , Added eventListener to buttons

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+const buttonContainer=document.querySelector("#button-container");
+const display=document.querySelector("#display");
+const button=document.querySelectorAll('button');
+const operator=document.querySelectorAll("#left-operators button")
+const numbers=document.querySelectorAll(".numbers button")
+
+let num1=null,num2=null;
+let oper=null;
+let res=null;
+
+
+numbers.forEach((num)=>{
+    num.addEventListener("click",(e)=>{
+        
+        if(num1==undefined){num1=num.textContent;
+        console.log(`num1=${num1}`);}
+       else {num2=num.textContent;
+       console.log(`num2=${num2}`)}
+});
+})
+operator.forEach((op)=>{
+    op.addEventListener("click",(e)=>{
+        oper=op.textContent;
+        console.log(oper);
+    })
+})
+button.forEach((button)=>{
+button.addEventListener("click",(e)=>display.textContent=e.target.textContent);
+})
+
+const equals=document.querySelector("#equals")
+equals.addEventListener("click",()=>{
+    console.log(num1,num2,oper);
+    operate(num1,num2,oper)});
+
+
 function add(num1,num2){
-
     return num1+num2
-
 }
 
 function subtract(num1,num2){
     return num1-num2
-    
+  
 }
 
 function multiply(num1,num2){
@@ -19,17 +53,25 @@ function divide(num1,num2){
 }
 
 function operate(num1,num2,operator){
+    num1=Number(num1)
+    num2=Number(num2)
+    
+    
     if(operator=="+"){
-        add(num1,num2)
+       res= add(num1,num2)
     }
     if(operator=="-"){
-        subtract(num1,num2)
+       res= subtract(num1,num2)
     }
     if(operator=="*"){
-        multiply(num1,num2)
+       res= multiply(num1,num2)
     }
     if(operator=="/"){
-        divide(num1,num2)
+        res=divide(num1,num2)
     }
+
+    display.textContent=res;
+
+  
 
 }
