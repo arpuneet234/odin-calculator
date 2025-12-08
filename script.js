0e5137b315a24519a87180251e04298f01e50508
Puneet Arora <arorapuneet234@gmail.com>
2025-12-08 16:19:14 +0530

    started again made layout using flex

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-const buttonContainer=document.querySelector("#button-container");
-const display=document.querySelector("#display");
-const button=document.querySelectorAll('button');
-const operator=document.querySelectorAll("#left-operators button")
-const numbers=document.querySelectorAll(".numbers button")
-
-let num1=null,num2=null;
-let oper=null;
-let res=null;
-
-
-numbers.forEach((num)=>{
-    num.addEventListener("click",(e)=>{
-        
-        if(num1==undefined){num1=num.textContent;
-        console.log(`num1=${num1}`);}
-       else {num2=num.textContent;
-       console.log(`num2=${num2}`)}
-});
-})
-operator.forEach((op)=>{
-    op.addEventListener("click",(e)=>{
-        oper=op.textContent;
-        console.log(oper);
-    })
-})
-button.forEach((button)=>{
-button.addEventListener("click",(e)=>display.textContent=e.target.textContent);
-})
-
-const equals=document.querySelector("#equals")
-equals.addEventListener("click",()=>{
-    console.log(num1,num2,oper);
-    operate(num1,num2,oper)});
-
-
 function add(num1,num2){
     return num1+num2
 }
@@ -69,9 +33,6 @@ function operate(num1,num2,operator){
     if(operator=="/"){
         res=divide(num1,num2)
     }
-
-    display.textContent=res;
-
-  
-
+     console.log(res);
+    
 }
