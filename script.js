85c25a132e6b1d730e9d12a9a3e7bc8685b8060c
Puneet Arora <arorapuneet234@gmail.com>
2025-12-09 14:27:14 +0530

    Corrected logic

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let num1 = null,
   num2 = null,
   res = null,
   oper = null;
-justCalculated = false;
+let justCalculated = false;
+let opAfterJustCalculated = false;
 
 buttons.forEach((button) => {
   button.addEventListener("click", fn);
@@ -13,22 +14,32 @@ buttons.forEach((button) => {
 function fn(e) {
   if (e.target.classList.contains("number")) {
     if (justCalculated == true) {
+      if (!opAfterJustCalculated) {
+        num1 = null;
+        res = null;
+        oper = null;
+      }
       display.textContent = "";
       justCalculated = false;
-      num1 = null;
       num2 = null;
-      res = null;
-      oper = null;
     }
     display.textContent += e.target.textContent;
     //   num1 = num1 * 10 + Number(button.textContent);
+  } else if (e.target.id == "decimal") {
+    if (display.textContent == "") display.textContent = 0;
+    if (!display.textContent.includes(".")) {
+      display.textContent += e.target.textContent;
+    }
   } else if (e.target.classList.contains("operator")) {
     if (justCalculated == true) {
-      display.textContent = "";
-      justCalculated = false;
+      //   display.textContent = "";
+      //   justCalculated = false;
+      opAfterJustCalculated = true;
       num1 = res;
+      console.log("num1 inside if of operator ", num1);
     }
     oper = e.target.textContent;
+    console.log(oper);
     if (num1 == null) {
       num1 = Number(display.textContent);
       display.textContent = "";
@@ -40,9 +51,11 @@ function fn(e) {
     if (justCalculated) {
       num1 = res;
     }
-
-    operate(num1, num2, oper);
+    if (num1 != null && num2 != null) {
+      operate(num1, num2, oper);
+    }
   } else if (e.target.id == "ce") {
+    opAfterJustCalculated = false;
     display.textContent = "";
     justCalculated = false;
     num1 = null;
@@ -72,9 +85,9 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operator) {
-  console.log(num1);
+  console.log("num1:", num1);
   console.log(operator);
-  console.log(num2);
+  console.log("num2:", num2);
 
   if (operator == "+") {
     res = add(num1, num2);
